ad7976521a4b12917a326d225b1fe8ba58fb7a53
ChamodDulanjana <chamodperera128@gmail.com>
2025-03-02 22:49:56 +0530

    Installing tailwind CSS!

---
+++ src/pages/home/page.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-blue-400 text-lg flex justify-center items-center">Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
