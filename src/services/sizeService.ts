b22965ec032a504867665c42031d1aa7fe3be950
chamoddulanjana <chamodperera128@gmail.com>
2025-04-30 23:13:48 +0530

    .env file updated

--- src/services/sizeService.ts
+++ src/services/sizeService.ts
@@ -1,10 +1,12 @@
-import axiosInstance from '../api/axiosInstance.ts';
+import axios from "axios";
 
+const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL ?? '';
 const SUB_URL: string = '/size';
 
 export const getAllActiveSizes = async () => {
     try {
-        const response = await axiosInstance.get(SUB_URL + '/all/active-sizes');
+        const response = await axios.get(API_BASE_URL + SUB_URL + '/all/active-sizes');
+        console.log(response);
         return response.data.data;
     } catch (error) {
         console.error('Error fetching all active sizes:', error);
