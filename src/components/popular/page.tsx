2bb0be8bfad3c40029939824ddf914718898121e
chamoddulanjana <chamodperera128@gmail.com>
2025-06-04 18:46:11 +0530

    install carousel from shadCn

--- src/components/popular/page.tsx
+++ src/components/popular/page.tsx
@@ -1,9 +1,12 @@
 
-
 const Popular = () => {
     return (
-        <div className="w-full h-screen flex items-start justify-center bg-white">
-            <h1 className="text-2xl font-poppins mt-16 whitespace-nowrap">Popular Items</h1>
+        <div className="w-full min-h-screen flex flex-col items-start justify-start bg-white py-16 px-10 font-poppins">
+            <h1 className="text-3xl font-bold whitespace-nowrap mb-4">Popular Items</h1>
+            <p className='text-sm max-w-lg leading-6'>
+                Explore our most-loved styles in one place. From best-selling outfits to must-have accessories,
+                discover what everyone’s talking about and upgrade your look with the trendiest picks.
+            </p>
         </div>
     );
 };
