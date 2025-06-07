b75c286bf8d8c731627d5156c38c71d3af45834c
chamoddulanjana <chamodperera128@gmail.com>
2025-06-07 12:20:25 +0530

    hero components modified

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -3,7 +3,7 @@
 const Hero = () => {
     return (
         <div className="w-full h-screen flex items-center justify-center bg-transparent overflow-auto">
-            <span className="flex justify-center items-center text-[80px] sm:text-[150px] lg:text-[250px] xl:text-[350px] font-light tracking-wider text-white h-full font-abel">
+            <span className="flex justify-center items-center text-[80px] sm:text-[150px] lg:text-[250px] xl:text-[350px] min-[2560px]:text-[480px] font-light tracking-wider text-white h-full font-abel">
                 MARNON
             </span>
         </div>
