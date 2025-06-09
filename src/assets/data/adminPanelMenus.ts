7e7d7a66b6d740fae3398517b1ba5e76d62b6240
chamoddulanjana <chamodperera128@gmail.com>
2025-06-09 09:16:11 +0530

    create admin menus array

---
+++ src/assets/data/adminPanelMenus.ts
@@ -0,0 +1,35 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+
+export const menus = [
+  {
+    title: "Dashboard",
+    url: "/admin-panel/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Products",
+    url: "/admin-panel/products",
+    icon: Inbox,
+  },
+  {
+    title: "Category",
+    url: "/admin-panel/category",
+    icon: Calendar,
+  },
+  {
+    title: "Sizes",
+    url: "/admin-panel/sizes",
+    icon: Search,
+  },
+  {
+    title: "Users",
+    url: "/admin-panel/users",
+    icon: Settings,
+  },
+  {
+    title: "Home Page Images",
+    url: "/admin-panel/home-page-images",
+    icon: Settings,
+  },
+]
